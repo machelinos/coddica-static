ed5eceae1a1265f74cfb2300f5f58b29e890d042
Marcel Cabrera <marcel.cabrera@gmail.com>
2013-06-08 12:09:27 -0500

    adelantos coddica

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
 	$(".flexnav").flexNav({ 'animationSpeed' : 'fast' });
+
+	$('#slider-id').liquidSlider({
+		mobileNavigation:false,
+		autoHeight:true,
+		slideEaseDuration:600
+	});
+
+
+	$('.buscar-boton').click(function(){
+		$('.busqueda').slideToggle('fast');
+	});
+
+});
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    	controlNav: false
+    });
+  });
+
+$(window).bind("load resize", function(){
+  var container_width = $('#fb-contenedor').width();
+    $('#fb-contenedor').html('<div class="fb-like-box" ' +
+    'data-href="https://www.facebook.com/pages/C%C3%B3ddica/166567273493678"' +
+    ' data-width="' + container_width + '" data-height="350" data-show-faces="false" ' +
+    'data-stream="true" data-header="true"></div>');
+    FB.XFBML.parse( );
 });
